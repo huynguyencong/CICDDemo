58a5afba26fd5040481d4abccc46426988eb088f
Huy Nguyen <conghuy2012@gmail.com>
2020-12-02 15:26:59 +0700

    Init project

---
+++ CICDDemo/CICDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDDemoApp.swift
+//  CICDDemo
+//
+//  Created by Huy Nguyen on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct CICDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
