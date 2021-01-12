9cb76ae0be0e1ac44f63c18eaf7a8aa806f3a46c
Huy Nguyen <conghuy2012@gmail.com>
2021-01-12 11:04:18 +0700

    Update text

--- CICDDemo/ContentView.swift
+++ CICDDemo/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 HStack {
-                    TextField("B 1", text: $number1Text)
+                    TextField("Input 1", text: $number1Text)
                         .keyboardType(.decimalPad)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     
